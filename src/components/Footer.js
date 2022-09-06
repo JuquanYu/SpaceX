dbdc1462a2ed34dade4b434b7b06a2279ae10756
JuquanYu <93880334+JuquanYu@users.noreply.github.com>
2022-09-06 18:10:46 -0400

    init

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <footer className="footer">
+                ©2022 StarLink Tracker. All Rights Reserved. Website Made by Juquan Yu.
+            </footer>
+        );
+    }
+}
+
+export default Footer;
+
